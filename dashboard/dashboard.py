b955422e0daa93e059d23b37abf9df466572b5d4
Enchilada429 <omaryahya.contact@gmail.com>
2026-01-26 16:21:34 +0000

    Add file structure

---
+++ dashboard/dashboard.py
@@ -0,0 +1,4 @@
+"""Script for hosting the dashboard for the past 24 hours of data."""
+
+if __name__ == "__main__":
+    ...
---
+++ pipeline/extract.py
@@ -0,0 +1,4 @@
+"""Script for extracting data from the API."""
+
+if __name__ == "__main__":
+    ...
