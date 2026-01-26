6fc3a7bb5c26fd1ede67fc4a7d01a66c9d79eb59
Enchilada429 <omaryahya.contact@gmail.com>
2026-01-26 18:17:26 +0000

    Add basic connection to database.

--- pipeline/extract.py
+++ pipeline/extract.py
@@ -1,4 +1,22 @@
 """Script for extracting data from the API."""
 
+from os import environ as ENV, _Environ
+
+from dotenv import load_dotenv
+from mssql_python import connect, Connection
+
+
+def get_db_connection(config: _Environ) -> Connection:
+    """Returns a connection the SQL Server database."""
+
+    return connect(
+        f"Server={config["SERVER_NAME"]};Database=" + "{" + config["DATABASE_NAME"] + "}" +
+        ";Encrypt=yes;TrustServerCertificate=no;Authentication=ActiveDirectoryInteractive"
+    )
+
+
 if __name__ == "__main__":
-    ...
+
+    load_dotenv()
+
+    conn = get_db_connection(ENV)
