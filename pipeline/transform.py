b9380c8ee82c7cdd09ccdfbe23e931a77da1fe08
Basil <trainee.basil.wahab@sigmalabs.co.uk>
2026-01-27 12:30:51 +0000

    Added Error handline, outlier handling on transform script

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -1,5 +1,5 @@
 """Script for cleaning and modifying the extracted data."""
-from extract import extract_data
+from extract import get_all_plants_data
 import pandas as pd
 
 
@@ -7,6 +7,7 @@ import pandas as pd
 
 
 def transform_data(records: list[dict]) -> pd.DataFrame:
+    """Return pandas dataframe from a list of dictionaries containing plant data"""
     rows = []
 
     for r in records:
@@ -16,7 +17,7 @@ def transform_data(records: list[dict]) -> pd.DataFrame:
         rows.append({
             "plant_id": r["plant_id"],
             "plant_name": r["name"],
-            "scientific_name": "; ".join(r["scientific_name"]),
+            "scientific_name": "; ".join(r["scientific_name"]) if "scientific_name" in r and r["scientific_name"] else None,
 
             "botanist_name": r["botanist"]["name"],
             "botanist_email": r["botanist"]["email"],
@@ -45,12 +46,26 @@ def transform_data(records: list[dict]) -> pd.DataFrame:
         df["recording_taken"], errors="coerce")
     df["latitude"] = pd.to_numeric(df["latitude"], errors="coerce")
     df["longitude"] = pd.to_numeric(df["longitude"], errors="coerce")
+    df["soil_moisture"] = pd.to_numeric(df["soil_moisture"], errors="coerce")
+    df["temperature"] = pd.to_numeric(df["temperature"], errors="coerce")
+
+    sm_mean = df["soil_moisture"].mean()
+    sm_std = df["soil_moisture"].std()
+
+    temp_mean = df["temperature"].mean()
+    temp_std = df["temperature"].std()
+
+    df = df[
+        df["soil_moisture"].between(sm_mean - 2 * sm_std, sm_mean + 2 * sm_std) &
+        df["temperature"].between(
+            temp_mean - 2 * temp_std, temp_mean + 2 * temp_std)
+    ]
 
     return df
 
 
 if __name__ == "__main__":
-    data = extract_data()
+    data = get_all_plants_data()
     df_transformed = transform_data(data)
 
-    print(df_transformed.head(10))
+    print(df_transformed.head(50))
