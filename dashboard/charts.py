83392135b6b2d08f83078147017c4fbc4d9a4a48
sahmiin <farihasamin03@gmail.com>
2026-01-27 10:43:16 +0000

    Create charts file and add a title.

---
+++ dashboard/charts.py
@@ -0,0 +1,13 @@
+"""This script will have the base figures to be plotted and then displayed on the dashboard."""
+
+import pandas as pd
+import altair as alt
+
+def plot_temp_over_time(conn, plant_id, date_range):
+    """Plots temperature over time for each plant."""
+    print("Temperature graph goes here.")
+
+
+def plot_moisture_over_time(conn, plant_id, date_range):
+    """Plots soil moisture over time for each plant."""
+    print("Moisture graph goes here.")
--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,4 +1,9 @@
 """Script for hosting the dashboard for the past 24 hours of data."""
 
-if __name__ == "__main__":
-    ...
+import streamlit as st
+import pandas as pd
+
+from charts import plot_temp_over_time, plot_moisture_over_time
+
+st.title(" 🌱 LMNH Plant Monitoring Dashboard ")
+
