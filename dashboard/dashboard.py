be1fa20366fb4ed80744e50b3141243f441ec3f4
sahmiin <farihasamin03@gmail.com>
2026-01-29 14:18:26 +0000

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -6,7 +6,7 @@ from streamlit_autorefresh import st_autorefresh
 from dotenv import load_dotenv
 
 from load_data import load_data
-from charts import bar_chart
+from charts import bar_chart, plot_temp_over_time, plot_moisture_over_time
 
 def display_dashboard():
     """Outputs the main visualisations of the dashboard."""
@@ -26,7 +26,7 @@ def display_dashboard():
     )
 
     st.title(" 🌱 LMNH Plant Monitoring Dashboard ")
-
+    st.divider()
     st.subheader(" Alerts 🥀")
 
     col1, col2 = st.columns(2)
@@ -34,27 +34,23 @@ def display_dashboard():
     with col1:
         st.subheader("Temperature Extremes")
         st.altair_chart(
-            bar_chart(top_5_temp, "temperature", "common_name", "Top 5 Highest Temperatures"),
-            use_container_width=True,
+            bar_chart(top_5_temp, "temperature", "common_name", "Highest Temperatures")
         )
 
         st.altair_chart(
-            bar_chart(bottom_5_temp, "temperature", "common_name", "Top 5 Lowest Temperatures"),
-            use_container_width=True,
+            bar_chart(bottom_5_temp, "temperature", "common_name", "Lowest Temperatures")
         )
 
     with col2:
         st.subheader("Soil Moisture Extremes")
         st.altair_chart(
-            bar_chart(top_5_moist, "soil_moisture", "common_name", "Top 5 Highest Soil Moisture"),
-            use_container_width=True,
+            bar_chart(top_5_moist, "soil_moisture", "common_name", "Highest Soil Moisture")
         )
 
         st.altair_chart(
-            bar_chart(bottom_5_moist, "soil_moisture", "common_name", "Top 5 Lowest Soil Moisture"),
-            use_container_width=True,
+            bar_chart(bottom_5_moist, "soil_moisture", "common_name", "Lowest Soil Moisture")
         )
-
+    st.divider()
 
     st.sidebar.header("🌿 Plant Selection")
     plant = st.sidebar.selectbox(
@@ -62,6 +58,20 @@ def display_dashboard():
         sorted(plant_recordings["common_name"].dropna().unique())
     )
     plant_df = plant_recordings[plant_recordings["common_name"] == plant].sort_values("recording_taken")
+    
+    st.subheader(f"📈 {plant} – Last 24 Hours")
+
+    st.divider()
+    col1, col2 = st.columns(2)
+    with col1:
+        st.altair_chart(
+            plot_temp_over_time(plant_df)
+        )
+
+    with col2:
+        st.altair_chart(
+            plot_moisture_over_time(plant_df)
+        )
 
 if __name__ == '__main__':
     load_dotenv()
