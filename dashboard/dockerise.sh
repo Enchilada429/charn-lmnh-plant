01821c6fa96006d801ff426ce761827f2ee5b5d3
sahmiin <farihasamin03@gmail.com>
2026-01-29 09:43:35 +0000

    Update dockerfile and fix dockerise.sh

--- dashboard/dockerise.sh
+++ dashboard/dockerise.sh
@@ -7,6 +7,6 @@ aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS 
 
 docker build -t ${AWS_ECR_REPO} . --platform "linux/amd64" --provenance=false
 
-docker tag ${AWS_ECR_REPO}:latest ${AWS_ECR_REPO}.dkr.ecr.${AWS_REGION}.amazonaws.com/${AWS_ECR_REPO}:latest
+docker tag ${AWS_ECR_REPO}:latest ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${AWS_ECR_REPO}:latest
 
 docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${AWS_ECR_REPO}:latest
