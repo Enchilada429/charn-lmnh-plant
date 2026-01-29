6864976fd1715270869b3f93bb4e238c3e922da0
Enchilada429 <omaryahya.contact@gmail.com>
2026-01-29 10:46:50 +0000

    Remove unnecessary select queries and add typehints

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,5 +1,6 @@
 """Script for uploading the cleaned data to the RDS (SQL Server)."""
 
+from time import perf_counter
 from logging import getLogger, basicConfig, INFO
 from os import environ as ENV, _Environ
 
@@ -31,7 +32,7 @@ def get_db_connection(config: _Environ):
     return conn
 
 
-def get_or_create_country_id(cur, country_name: str) -> int:
+def get_or_create_country_id(cur: pyodbc.Cursor, country_name: str) -> int:
     """Gets or creates country_id"""
     cur.execute(
         "SELECT country_id FROM beta.country WHERE country_name = ?;",
@@ -42,18 +43,14 @@ def get_or_create_country_id(cur, country_name: str) -> int:
         return int(row[0])
 
     cur.execute(
-        "INSERT INTO beta.country (country_name) VALUES (?);",
+        "INSERT INTO beta.country (country_name) OUTPUT country_id VALUES (?);",
         (country_name,),
     )
 
-    cur.execute(
-        "SELECT country_id FROM beta.country WHERE country_name = ?;",
-        (country_name,),
-    )
     return int(cur.fetchone()[0])
 
 
-def get_or_create_botanist_id(cur, name, email, phone):
+def get_or_create_botanist_id(cur: pyodbc.Cursor, name: str, email: str, phone: str):
     """Gets or creates botanist_id"""
     cur.execute(
         "SELECT botanist_id FROM beta.botanist WHERE email = ?;",
@@ -64,18 +61,14 @@ def get_or_create_botanist_id(cur, name, email, phone):
         return int(row[0])
 
     cur.execute(
-        "INSERT INTO beta.botanist (botanist_name, email, phone) VALUES (?, ?, ?);",
+        "INSERT INTO beta.botanist (botanist_name, email, phone) OUTPUT botanist_id VALUES (?, ?, ?);",
         (name, email, phone),
     )
 
-    cur.execute(
-        "SELECT botanist_id FROM beta.botanist WHERE email = ?;",
-        (email,),
-    )
     return int(cur.fetchone()[0])
 
 
-def get_or_create_plant_id(cur, common_name: str, scientific_name: str | None) -> int:
+def get_or_create_plant_id(cur: pyodbc.Cursor, common_name: str, scientific_name: str | None) -> int:
     """Gets or creates plant_id"""
     cur.execute(
         "SELECT plant_id FROM beta.plant WHERE common_name = ?;",
@@ -86,18 +79,14 @@ def get_or_create_plant_id(cur, common_name: str, scientific_name: str | None) -
         return int(row[0])
 
     cur.execute(
-        "INSERT INTO beta.plant (common_name, scientific_name) VALUES (?, ?);",
+        "INSERT INTO beta.plant (common_name, scientific_name) OUTPUT plant_id VALUES (?, ?);",
         (common_name, scientific_name),
     )
 
-    cur.execute(
-        "SELECT plant_id FROM beta.plant WHERE common_name = ?;",
-        (common_name,),
-    )
     return int(cur.fetchone()[0])
 
 
-def get_or_create_image_id(cur, license: int, license_name: str, license_url: str, thumbnail: str | None) -> int:
+def get_or_create_image_id(cur: pyodbc.Cursor, license: int, license_name: str, license_url: str, thumbnail: str | None) -> int:
     """Gets or creates image_id"""
     cur.execute(
         "SELECT image_id FROM beta.plant_image WHERE licence = ?;",
@@ -108,18 +97,14 @@ def get_or_create_image_id(cur, license: int, license_name: str, license_url: st
         return int(row[0])
 
     cur.execute(
-        "INSERT INTO beta.plant_image (licence, licence_name, licence_url, thumbnail) VALUES (?, ?, ?, ?);",
+        "INSERT INTO beta.plant_image (licence, licence_name, licence_url, thumbnail) OUTPUT image_id VALUES (?, ?, ?, ?);",
         (license, license_name, license_url, thumbnail,),
     )
 
-    cur.execute(
-        "SELECT image_id FROM beta.plant_image WHERE licence = ?;",
-        (license,),
-    )
     return int(cur.fetchone()[0])
 
 
-def get_or_create_origin_location_id(cur, city: str, country_id: int, longitude, latitude) -> int:
+def get_or_create_origin_location_id(cur: pyodbc.Cursor, city: str, country_id: int, longitude, latitude) -> int:
     """Gets or creates location_id"""
     cur.execute(
         """
@@ -136,25 +121,21 @@ def get_or_create_origin_location_id(cur, city: str, country_id: int, longitude,
     cur.execute(
         """
         INSERT INTO beta.origin_location (origin_city_name, country_id, longitude, latitude)
+        OUTPUT origin_location_id
         VALUES (?, ?, ?, ?);
         """,
         (city, country_id, longitude, latitude),
     )
 
-    cur.execute(
-        """
-        SELECT origin_location_id
-        FROM beta.origin_location
-        WHERE origin_city_name = ? AND country_id = ?;
-        """,
-        (city, country_id),
-    )
     return int(cur.fetchone()[0])
 
 
-def upload_recording_to_database(conn, recording) -> None:
+def upload_recording_to_database(conn: pyodbc.Connection, recording) -> None:
     """Inserts cleaned data into the database, creating any missing related records before inserting the final rows."""
-    logger.info(f"Starting upload of {len(recording)} recording.")
+
+    start_time = perf_counter()
+
+    logger.info(f"Upload started of {len(recording)} recordings.")
 
     recording_to_insert = []
 
@@ -213,8 +194,9 @@ def upload_recording_to_database(conn, recording) -> None:
         curs.executemany(query, recording_to_insert)
 
     conn.commit()
+
     logger.info(
-        f"Successfully inserted {len(recording_to_insert)} recording data.")
+        f"Upload finished of {len(recording_to_insert)} recording data with time taken: {perf_counter() - start_time} seconds.")
 
 
 if __name__ == "__main__":
